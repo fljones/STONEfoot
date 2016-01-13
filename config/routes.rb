9931903da457e29a2e50124e881a90895722f5ad
Frank Jones <fljones@containerstore.com>
2016-01-13 14:36:10 -0600

    Contined to style pages

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,11 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+    get '/' => 'welcome#index'
+    get '/music' => 'music#music'
+    get '/about' => 'about#about'
+    get '/blog' => 'blog#blog'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
